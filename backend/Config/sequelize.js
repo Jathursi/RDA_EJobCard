d08322a73420466be69895934c327f60d2e2fec7
jathursi <jathukirubajathu@gmail.com>
2024-12-01 14:09:33 +0530

    seq back

--- backend/Config/sequelize.js
+++ backend/Config/sequelize.js
@@ -1,13 +1,16 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, 
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectOptions: { connectTimeout: 60000 },
+    logging: false, // Disable logging to avoid clutter
+    dialectOptions: {
+      connectTimeout: 60000, // Timeout setting for slower connections
+    },
   }
 );
 
